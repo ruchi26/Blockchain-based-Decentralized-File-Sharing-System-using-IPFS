18ac4285630b4d373cfdbdddff27194d3937fe9e
Ruchika Tulsyan <truchika12@gmail.com>
2020-04-03 02:10:34 +0530

    Hashing the uploaded file using IPFS

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import urllib.request
+import ipfshttpclient
 from my_constants import app
 from flask import Flask, flash, request, redirect, render_template, request, url_for
 from werkzeug.utils import secure_filename
@@ -7,9 +8,13 @@ from werkzeug.utils import secure_filename
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
-
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def hashed(filename):
+    api = ipfshttpclient.connect('127.0.0.1', 5001)
+    res = api.add(filename)
+    return  res['Hash']
 
 @app.route('/')
 def home():
@@ -17,23 +22,25 @@ def home():
 
 @app.route('/', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
+    if request.method == 'POST':
         # check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-			return redirect(request.url)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            hashed_output = hashed(filename)
+            flash(hashed_output)
+            return redirect('/')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
