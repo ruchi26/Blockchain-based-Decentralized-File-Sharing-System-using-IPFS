fa9fc379794bf6b500a28b5e3f0c08eb0099a639
U-SHER\DELL <truchika12@gmail.com>
2020-04-16 23:33:41 +0530

    Corrected the add_file function

--- server.py
+++ server.py
@@ -2,10 +2,13 @@ import os
 import urllib.request
 import ipfshttpclient
 from my_constants import app
-from flask import Flask, flash, request, redirect, render_template, request, url_for
+from flask import Flask, flash, request, redirect, render_template, request, url_for, jsonify
 from werkzeug.utils import secure_filename
+from blockchain import Blockchain
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+blockchain = Blockchain()
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -19,27 +22,31 @@ def hashed(filename):
 def home():
     return render_template('first.html')
 
-@app.route('/', methods=['POST'])
-def upload_file():
+@app.route('/add_file', methods=['POST'])
+def add_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File successfully uploaded')
-            hashed_output1 = hashed(filename)
-            flash(hashed_output1)
-            return redirect('/')
+            message = 'No file part'
         else:
-            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-            return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':
+                message = 'No file selected for uploading'
+
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                message = 'File successfully uploaded'
+                file_hash = 'ABDEFgh'
+            # hashed_output1 = hashed(filename)
+                sender = 'Ruchika'
+                receiver = 'Souvik'
+                index = blockchain.add_file(sender, receiver, file_hash)
+                message = f'This file will be added to Block {index}'
+            else:
+                message = 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'
+        response = {'message': message}
+        return jsonify(response),  201
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
