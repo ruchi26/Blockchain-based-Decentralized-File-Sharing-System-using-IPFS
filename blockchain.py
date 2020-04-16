5452002d7a12d64e841286c8a46d921ab2bc7631
U-SHER\DELL <truchika12@gmail.com>
2020-04-16 23:48:21 +0530

    Commented out the routes in blockchain.py file

--- blockchain.py
+++ blockchain.py
@@ -11,7 +11,6 @@ import hashlib
 import json
 from flask import Flask, jsonify, request
 import requests
-from uuid import uuid4
 from urllib.parse import urlparse
 
 # Part 1 - Building a Blockchain
@@ -73,7 +72,13 @@ class Blockchain:
                                   'receiver': receiver,
                                   'file_hash': file_hash})
         previous_block = self.get_previous_block()
-        return previous_block['index'] + 1
+        index = previous_block['index'] + 1
+        if len(self.shared_files) == 10:
+            previous_proof = previous_block['proof']
+            proof = self.proof_of_work(previous_proof)
+            previous_hash = self.hash(previous_block)
+            block = self.create_block(proof, previous_hash)
+        return index
     
     def add_node(self, address):
         parsed_url = urlparse(address)
@@ -99,84 +104,79 @@ class Blockchain:
 # Part 2 - Mining our Blockchain
 
 # Creating a Web App
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# Creating an address for the node on Port 5000
-node_address = str(uuid4()).replace('-', '')
+# # Mining a new block
+# @app.route('/mine_block', methods = ['GET'])
+# def mine_block():
+#     previous_block = blockchain.get_previous_block()
+#     previous_proof = previous_block['proof']
+#     proof = blockchain.proof_of_work(previous_proof)
+#     previous_hash = blockchain.hash(previous_block)
+#     block = blockchain.create_block(proof, previous_hash)
+#     response = {'message': 'Congratulations, you just mined a block!',
+#                 'index': block['index'],
+#                 'timestamp': block['timestamp'],
+#                 'proof': block['proof'],
+#                 'previous_hash': block['previous_hash'],
+#                 'shared_files': block['shared_files']}
+#     return jsonify(response), 200
 
-# Creating a Blockchain
-blockchain = Blockchain()
 
-# Mining a new block
-@app.route('/mine_block', methods = ['GET'])
-def mine_block():
-    previous_block = blockchain.get_previous_block()
-    previous_proof = previous_block['proof']
-    proof = blockchain.proof_of_work(previous_proof)
-    previous_hash = blockchain.hash(previous_block)
-    block = blockchain.create_block(proof, previous_hash)
-    response = {'message': 'Congratulations, you just mined a block!',
-                'index': block['index'],
-                'timestamp': block['timestamp'],
-                'proof': block['proof'],
-                'previous_hash': block['previous_hash'],
-                'shared_files': block['shared_files']}
-    return jsonify(response), 200
+# # Getting the full Blockchain
+# @app.route('/get_chain', methods = ['GET'])
+# def get_chain():
+#     response = {'chain': blockchain.chain,
+#                 'length': len(blockchain.chain)}
+#     return jsonify(response), 200
 
-# Getting the full Blockchain
-@app.route('/get_chain', methods = ['GET'])
-def get_chain():
-    response = {'chain': blockchain.chain,
-                'length': len(blockchain.chain)}
-    return jsonify(response), 200
-
-# Checking if the Blockchain is valid
-@app.route('/is_valid', methods = ['GET'])
-def is_valid():
-    is_valid = blockchain.is_chain_valid(blockchain.chain)
-    if is_valid:
-        response = {'message': 'All good. The Blockchain is valid.'}
-    else:
-        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
-    return jsonify(response), 200
+# # Checking if the Blockchain is valid
+# @app.route('/is_valid', methods = ['GET'])
+# def is_valid():
+#     is_valid = blockchain.is_chain_valid(blockchain.chain)
+#     if is_valid:
+#         response = {'message': 'All good. The Blockchain is valid.'}
+#     else:
+#         response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+#     return jsonify(response), 200
 
 # Adding a new file to the Blockchain
-@app.route('/add_file', methods = ['POST'])
-def add_file():
-    json = request.get_json()
-    file_keys = ['sender', 'receiver', 'file_hash']
-    if not all(key in json for key in file_keys):
-        return 'Some details of the file are missing', 400
-    index = blockchain.add_file(json['sender'], json['receiver'], json['file_hash'])
-    response = {'message': f'This file will be added to Block {index}'}
-    return jsonify(response), 201
+# @app.route('/add_file', methods = ['POST'])
+# def add_file():
+#     json = request.get_json()
+#     file_keys = ['sender', 'receiver', 'file_hash']
+#     if not all(key in json for key in file_keys):
+#         return 'Some details of the file are missing', 400
+#     index = blockchain.add_file(json['sender'], json['receiver'], json['file_hash'])
+#     response = {'message': f'This file will be added to Block {index}'}
+#     return jsonify(response), 201
 
 # Part 3 - Decentralizing our Blockchain
 
 # Connecting new nodes
-@app.route('/connect_node', methods = ['POST'])
-def connect_node():
-    json = request.get_json()
-    nodes = json.get('nodes')
-    if nodes is None:
-        return "No node", 400
-    for node in nodes:
-        blockchain.add_node(node)
-    response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
-                'total_nodes': list(blockchain.nodes)}
-    return jsonify(response), 201
+# @app.route('/connect_node', methods = ['POST'])
+# def connect_node():
+#     json = request.get_json()
+#     nodes = json.get('nodes')
+#     if nodes is None:
+#         return "No node", 400
+#     for node in nodes:
+#         blockchain.add_node(node)
+#     response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
+#                 'total_nodes': list(blockchain.nodes)}
+#     return jsonify(response), 201
 
-# Replacing the chain by the longest chain if needed
-@app.route('/replace_chain', methods = ['GET'])
-def replace_chain():
-    is_chain_replaced = blockchain.replace_chain()
-    if is_chain_replaced:
-        response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
-                    'new_chain': blockchain.chain}
-    else:
-        response = {'message': 'All good. The chain is the largest one.',
-                    'actual_chain': blockchain.chain}
-    return jsonify(response), 200
+# # Replacing the chain by the longest chain if needed
+# @app.route('/replace_chain', methods = ['GET'])
+# def replace_chain():
+#     is_chain_replaced = blockchain.replace_chain()
+#     if is_chain_replaced:
+#         response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
+#                     'new_chain': blockchain.chain}
+#     else:
+#         response = {'message': 'All good. The chain is the largest one.',
+#                     'actual_chain': blockchain.chain}
+#     return jsonify(response), 200
 
-# Running the app
-app.run(host = '0.0.0.0', port = 5000)
+# # Running the app
+# app.run(host = '0.0.0.0', port = 5000)
