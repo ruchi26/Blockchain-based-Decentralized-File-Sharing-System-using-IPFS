806edc67cad2d35c7113b90a45533785506ce183
U-SHER\DELL <truchika12@gmail.com>
2020-04-25 00:16:12 +0530

    Extracting the contents of a file and then sending a post request using  the file contents

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ from my_constants import app
 from flask import Flask, flash, request, redirect, render_template, url_for, jsonify
 from werkzeug.utils import secure_filename
 from blockchain import Blockchain
-import requests as request_file_hash
+import requests
 
-                # This package is used in the 'hashed' function while getting the hash of the file.
-                # Notice that this package , 'requests' is different than the package 'request'.
-                # 'request' package is used in the 'add_file' function for multiple actions.
+# The package requests is used in the 'hash_user_file' and 'retrieve_from hash' functions to send http post requests.
+# Notice that 'requests' is different than the package 'request'.
+# 'request' package is used in the 'add_file' function for multiple actions.
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
@@ -18,22 +18,18 @@ blockchain = Blockchain()
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def hashed(filename):
+def hash_user_file(file_content):
     url = 'https://ipfs.infura.io:5001/api/v0/add'
-    user_file = { 'file' : (filename), }
-    response = request_file_hash.post(url, files = user_file)
-    #print(p['Hash'])
-    # client = ipfshttpclient.connect('/dns/ipfs.infura.io/tcp/5001/https')
-    # result = client.add(filename)
+    user_file = { 'file' : (file_content), }
+    response = requests.post(url, files = user_file)
     hashed_file = response.json()['Hash']
-    return  hashed_file
+    return hashed_file
 
 def retrieve_from_hash(file_hash):
-    url = 'https://ipfs.infura.io:5001/api/v0/block/get'
-    response = request_file_hash.post(url, key = file_hash)
-    print(response)
-    user_file = response
-    return user_file
+    url = 'https://ipfs.infura.io:5001/api/v0/cat?arg=' + file_hash
+    response = requests.get(url)
+    file_content = response.text
+    return file_content
 
 @app.route('/')
 def home():
@@ -42,23 +38,21 @@ def home():
 @app.route('/add_file', methods=['POST'])
 def add_file():
     if request.method == 'POST':
-
         error_flag = True
-
-        # check if the post request has the file part
         if 'file' not in request.files:
             message = 'No file part'
         else:
-            file = request.files['file']
-            if file.filename == '':
+            user_file = request.files['file']
+            if user_file.filename == '':
                 message = 'No file selected for uploading'
 
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            if user_file and allowed_file(user_file.filename):
+                filename = secure_filename(user_file.filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                user_file.save(file_path)
                 message = 'File successfully uploaded'
-                #file_hash = 'ABDEFgh'
-                hashed_output1 = hashed(filename)
+                file_content = open(file_path, 'rb').read()
+                hashed_output1 = hash_user_file( file_content)
                 sender = request.form['sender_name']
                 receiver = request.form['receiver_name']
                 index = blockchain.add_file(sender, receiver, hashed_output1)
@@ -84,20 +78,18 @@ def retrieve_file():
 
         if request.form['file_hash'] == '':
             message = 'No hash entered'
-        if len(request.form['file_hash']) != 64:
-            message = 'Incorrect hash'
+        #if len(request.form['file_hash']) != 64:
+        #   message = 'Incorrect hash '+ request.form['file_hash']
         else:
             error_flag = False
             file_hash = request.form['file_hash']
-            user_file = retrieve_from_hash(file_hash)
-            print(user_file)
+            file_content = retrieve_from_hash(file_hash)
+            message = 'File successfully retrieved'
 
         if error_flag == True:
-            return render_template('first.html', messages = {'message' : message})
+            return render_template('second.html', messages = {'message' : message , 'blockchain' : file_content})
         else:
-            return render_template('second.html',messages = {'message' : message})
-
-
+            return render_template('second.html',messages = {'message' : message , 'blockchain' : file_content})
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
